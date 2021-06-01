0c058d9260a08793a04ec1103f372175d293bc8f
gutialv23 <alvarogarcia.g23@gmail.com>
2021-06-01 02:30:25 +0200

    EM10 Starting register process and error logs

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { useHistory } from 'react-router-dom';
 
 // Elements.
 import { Header, HeaderTitle } from '../elements/Header';
 import { Button, ButtonContainer } from '../elements/Buttons';
 import { Form, Input, InputContainer, SvgRegister } from '../elements/FormInputs';
 
+// Firebase.
+import { auth } from '../firebase/firebaseConfig';
+
 const Register = () =>
 {
+    const history = useHistory();
+
+    const [ email    , updateEmail     ] = useState( "" );
+    const [ password , updatePassword  ] = useState( "" );
+    const [ password2, updatePassword2 ] = useState( "" );
+
+    const handleSubmit = async ( e ) =>
+    {
+        e.preventDefault();
+
+        const regExp = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+
+        // Check that all the fields have been filled.
+
+        if ( ( email     === "" ) ||
+             ( password  === "" ) ||
+             ( password2 === "" ) )
+        {
+            console.log( "ERROR: Please fill all the fields." );
+        }
+
+        // Check that the email is valid.
+
+        else if ( !regExp.test( email ) )
+        {
+            console.log( "ERROR: your email is not valid." );
+        }
+
+        // Check that passwords match.
+
+        else if ( password !== password2 )
+        {
+            console.log( "ERROR: passwords do not match." );
+        }
+        else
+        {
+            // Everything is OK -> Create user.
+
+            try
+            {
+                await auth.createUserWithEmailAndPassword( email, password );
+
+                history.push( "/" );
+            }
+            catch ( error ) { console.log( error ); }
+
+            // TODO: Unify errors?
+        }
+    }
+
     return (
         <>
             <Helmet>
@@ -19,13 +73,13 @@ const Register = () =>
                 <Button to="/login">Login</Button>
             </Header>
 
-            <Form>
+            <Form onSubmit={ handleSubmit }>
                 <SvgRegister />
 
                 <InputContainer>
-                    <Input type="email"    name="email"     placeholder="Email"           />
-                    <Input type="password" name="password"  placeholder="Password"        />
-                    <Input type="password" name="password2" placeholder="Repeat password" />
+                    <Input type="email"    name="email"     placeholder="Email"           value={ email     } onChange={ ( e ) => updateEmail    ( e.target.value ) } />
+                    <Input type="password" name="password"  placeholder="Password"        value={ password  } onChange={ ( e ) => updatePassword ( e.target.value ) } />
+                    <Input type="password" name="password2" placeholder="Repeat password" value={ password2 } onChange={ ( e ) => updatePassword2( e.target.value ) } />
                 </InputContainer>
 
                 <ButtonContainer center >
